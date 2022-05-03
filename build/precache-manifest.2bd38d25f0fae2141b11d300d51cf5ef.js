448d06e67c68a0371968fa4778293164d7edf20f
hitesh-mishra-456 <hiteshm2202@gmail.com>
2022-05-03 16:10:26 +0530

    Final Commit

---
+++ build/precache-manifest.2bd38d25f0fae2141b11d300d51cf5ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e53539603af502109cb46a4b51a4c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "370656df81f0df85591f",
+    "url": "/static/css/main.9516a8fc.chunk.css"
+  },
+  {
+    "revision": "0a115ace587cd3cb1ff3",
+    "url": "/static/js/2.ee0aebaa.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.ee0aebaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "370656df81f0df85591f",
+    "url": "/static/js/main.5842de35.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
